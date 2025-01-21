2da4900111322a748fce07b5f290fcb6a4ec37df
Aditya Kirubakaran <131557059+adityakirubakaran@users.noreply.github.com>
2025-01-21 17:41:55 -0500

    Add files via upload

---
+++ cliques.py
@@ -0,0 +1,169 @@
+import numpy as np
+from itertools import combinations
+
+def find_cliques_from_matrix(adj_matrix, names):
+    # Square the adjacency matrix
+    squared_matrix = np.dot(adj_matrix, adj_matrix)
+
+    # Find cliques of size 3 (triangles)
+    num_vertices = len(adj_matrix)
+    cliques = []
+    
+    for i in range(num_vertices):
+        for j in range(i + 1, num_vertices):  # Avoid double counting
+            if squared_matrix[i][j] > 0:  # Shared neighbors exist
+                # Verify clique condition: all neighbors are connected
+                common_neighbors = [k for k in range(num_vertices) 
+                                    if adj_matrix[i][k] == 1 and adj_matrix[j][k] == 1]
+                for k in common_neighbors:
+                    if adj_matrix[i][j] == 1 and adj_matrix[i][k] == 1 and adj_matrix[j][k] == 1:
+                        cliques.append((names[i], names[j], names[k]))
+    
+    return cliques
+    
+def find_cliques_of_size_4(adj_matrix, names):
+    num_vertices = len(adj_matrix)
+    cliques = []
+
+    # Check all combinations of 4 nodes
+    for combination in combinations(range(num_vertices), 4):
+        i, j, k, l = combination
+
+        # Check if every pair in the combination is connected
+        if (adj_matrix[i][j] == 1 and adj_matrix[i][k] == 1 and adj_matrix[i][l] == 1 and
+            adj_matrix[j][k] == 1 and adj_matrix[j][l] == 1 and
+            adj_matrix[k][l] == 1):
+            cliques.append((names[i], names[j], names[k], names[l]))
+    
+    return cliques
+
+def find_cliques_of_size_5(adj_matrix, names):
+    num_vertices = len(adj_matrix)
+    cliques = []
+
+    # Check all combinations of 5 nodes
+    for combination in combinations(range(num_vertices), 5):
+        a, b, c, d, e = combination
+
+        # Check if every pair in the combination is connected
+        if (adj_matrix[a][b] == 1 and adj_matrix[a][c] == 1 and adj_matrix[a][d] == 1 and adj_matrix[a][e] == 1 and
+            adj_matrix[b][c] == 1 and adj_matrix[b][d] == 1 and adj_matrix[b][e] == 1 and
+            adj_matrix[c][d] == 1 and adj_matrix[c][e] == 1 and
+            adj_matrix[d][e] == 1):
+            cliques.append((names[a], names[b], names[c], names[d], names[e]))
+    
+    return cliques
+
+def find_cliques_of_size_6(adj_matrix, names):
+    num_vertices = len(adj_matrix)
+    cliques = []
+
+    # Check all combinations of 6 nodes
+    for combination in combinations(range(num_vertices), 6):
+        a, b, c, d, e, f = combination
+
+        # Check if every pair in the combination is connected
+        if (adj_matrix[a][b] == 1 and adj_matrix[a][c] == 1 and adj_matrix[a][d] == 1 and adj_matrix[a][e] == 1 and adj_matrix[a][f] == 1 and
+            adj_matrix[b][c] == 1 and adj_matrix[b][d] == 1 and adj_matrix[b][e] == 1 and adj_matrix[b][f] == 1 and
+            adj_matrix[c][d] == 1 and adj_matrix[c][e] == 1 and adj_matrix[c][f] == 1 and
+            adj_matrix[d][e] == 1 and adj_matrix[d][f] == 1 and
+            adj_matrix[e][f] == 1):
+            cliques.append((names[a], names[b], names[c], names[d], names[e], names[f]))
+    
+    return cliques
+
+
+def find_cliques_of_size_7(adj_matrix, names):
+    num_vertices = len(adj_matrix)
+    cliques = []
+
+    # Check all combinations of 7 nodes
+    for combination in combinations(range(num_vertices), 7):
+        a, b, c, d, e, f, g = combination
+
+        # Check if every pair in the combination is connected
+        if (adj_matrix[a][b] == 1 and adj_matrix[a][c] == 1 and adj_matrix[a][d] == 1 and adj_matrix[a][e] == 1 and adj_matrix[a][f] == 1 and adj_matrix[a][g] == 1 and
+            adj_matrix[b][c] == 1 and adj_matrix[b][d] == 1 and adj_matrix[b][e] == 1 and adj_matrix[b][f] == 1 and adj_matrix[b][g] == 1 and
+            adj_matrix[c][d] == 1 and adj_matrix[c][e] == 1 and adj_matrix[c][f] == 1 and adj_matrix[c][g] == 1 and
+            adj_matrix[d][e] == 1 and adj_matrix[d][f] == 1 and adj_matrix[d][g] == 1 and
+            adj_matrix[e][f] == 1 and adj_matrix[e][g] == 1 and
+            adj_matrix[f][g] == 1):
+            cliques.append((names[a], names[b], names[c], names[d], names[e], names[f], names[g]))
+    
+    return cliques
+
+
+# Example adjacency matrix
+adj_matrix = np.array([
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+[0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0],
+[0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+[0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0],
+[0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0],
+[1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+[0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+[0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+[1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+[0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0],
+[0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+[0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+[0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+[0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+])
+
+# Corresponding names for each node
+names = [
+    "Abhay Bhaskar", "Abhishek Terala", "Akshada Guruvayur", "Aditya Kirubakaran",
+    "Amruta Jayaganesh", "Archisa Arora", "Archisha Bhattacharyya", "Archith Raman",
+    "Ayush Tripathi", "Brian Wilk", "Dakshata Ravinder", "Emma Contursi",
+    "Ethan Poon", "Ishaan Ghosh", "Joshua Verma", "Julia Diaz",
+    "Luca Bonagura", "Mahanth Komuravelli", "Martin Zhang", "Naisha Bhandari",
+    "Nandini Venkatesh", "Nimay Sathees", "Nishi Agrawal", "Nova Travis",
+    "Priyadarshan Kabra", "Rahil Patel", "Sahasra Kuravarapu", "SaiSriVastav Minnal",
+    "Saniya Grover", "Sanjay Ravishankar", "Sargam Mondal", "Satvika Singh",
+    "Shelby Hilarczyk", "Shreya Iyer", "Shreya Kuthpadi", "Soumya Singh",
+    "Sounak Bagchi", "Srivarun Kankanala", "Suhani Sengupta", "Sukhveen Gill",
+    "Thomas Catuosco"
+]
+
+cliques = find_cliques_from_matrix(adj_matrix, names)
+print("Cliques:", cliques)
+
+cliques = find_cliques_of_size_4(adj_matrix, names)
+print("Cliques of size 4:", cliques)
+
+cliques = find_cliques_of_size_5(adj_matrix, names)
+print("Cliques of size 5:", cliques)
+
+cliques = find_cliques_of_size_6(adj_matrix, names)
+print("Cliques of size 6:", cliques)
+
+cliques = find_cliques_of_size_7(adj_matrix, names)
+print("Cliques of size 7:", cliques)
